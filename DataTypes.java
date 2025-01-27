e150f5549881a5e2bd573873f6a4694caa80e3d5
Devendra <devendramallarapu8@gmail.com>
2025-01-27 18:27:28 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num = 25 ;
+        char ch = 'a';
+        float f = 2.3f;
+        double d= 4.865947;
+        System.out.println("Number :"+num);
+        System.out.println("char :"+ch);
+        System.out.println("Float :"+f);
+        System.out.println("double :"+d);
+    }
+}
